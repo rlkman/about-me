7f59730bfe7ee1c049672310df16e07c76f12ce4
Liberty <rlkmandisodza@gmail.com>
2017-11-12 22:52:16 -0800

    final submission for web page published

--- app.js
+++ app.js
@@ -3,56 +3,56 @@
 //Welcomes the user interactivelly
 var userName = prompt('Hi there, Welcome to my webpage. Could you please fill in your name and answer the following fun quizes');
 console.log('User Name supplied:', userName);
-alert('Thank you '+ userName +' take yout time to know me.' );
+alert('Thank you '+ userName +' take part in the following activities to get to know me.' );
 
 
 //The guessing game starts here
 
 //Question1
-var continentsVisited = prompt('Have I visited all 5 continents?');
+var continentsVisited = prompt('Have I visited all 5 continents?').toLowerCase();
 console.log('will accept yes or no answer:', continentsVisited);
 
-if(continentsVisited.toLowerCase() ==='yes'|| continentsVisited.toUpperCase() === 'YES' ){
+if(continentsVisited ==='yes'|| continentsVisited === 'y' ){
   alert('Nope, '+ userName +' I am still to visit a couple..!!');
 } else {
   alert('Hey '+userName+' that\'s true I am still to do that.');
 }
 
 //Question2
-var favouriteMovie = prompt('Do I have a favourite movie?');
+var favouriteMovie = prompt('Do I have a favourite movie?').toLowerCase();
 console.log('will accept yes or no answer:', favouriteMovie);
 
-if(favouriteMovie.toLowerCase() === 'no'|| favouriteMovie.toUpperCase() === 'NO' ){
+if(favouriteMovie === 'no'|| favouriteMovie === 'n' ){
   alert(userName + ' You are right, I dont like movies that much!!');
 } else {
   alert('That\'s unfortunately not true, '+ userName);
 }
 
 //Question3
-var petsOwned = prompt('Do I own any pets?');
+var petsOwned = prompt('Do I own any pets?').toLowerCase();
 console.log('will accept yes or no answer:', petsOwned);
 
-if(petsOwned.toLowerCase() === 'yes'|| petsOwned.toUpperCase() === 'YES' ){
+if(petsOwned === 'yes'|| petsOwned === 'y' ){
   alert('Nope, Unfotunately I love travelling too much to give enough attention to a pet');
 } else {
   alert('That\'s true I do not own a pet - I spend too much time on planes.');
 }
 
 //Question4
-var teamSupported = prompt('Do I support Liverpool Football Club?');
+var teamSupported = prompt('Do I support Liverpool Football Club?').toLowerCase();
 console.log('will accept yes or no answer:'+teamSupported);
 
-if(continentsVisited.toLowerCase() === 'no'|| continentsVisited.toUpperCase() === 'NO' ){
+if(continentsVisited === 'no'|| continentsVisited === 'n' ){
   alert('Are they any other teams...'+ userName + '???...YNWA.');
 } else {
   alert('Definitely, '+ userName +', I support Liverpool FC...YNWA...!!!');
 }
 
 //Question4
-var travelQuestion = prompt('Do I like travelling?');
+var travelQuestion = prompt('Do I like travelling?').toLowerCase();
 console.log('will accept yes or no answer:', travelQuestion);
 
-if(travelQuestion.toLowerCase() === 'yes'|| travelQuestion.toUpperCase() === 'YES' ){
+if(travelQuestion === 'yes'|| travelQuestion === 'y' ){
   alert('Definitely, '+ userName +', you bet...!!!');
 } else {
   alert('There\'s so much of the world to see.'+ userName + ', travelling is brilliant.');
@@ -86,7 +86,7 @@ for(var i = 0; i < 3; i++){
   }
 }
 
-alert('Thanks for playing the number guessing game, please continue.');
+alert('Thanks for playing the number guessing game, please continue on to the travelling game. The 14th is my birthday.');
 
 //Question7 - array of 6 correct answers that are checked on and gives tally at the enjoyed
 var placesVisited = ['iceland', 'norway', 'zimbabwe', 'England', 'canada', 'holland'];
@@ -96,14 +96,14 @@ var tally = 0;
 var chances = placesVisited.length;
 
 console.log('length of array:',placesVisited.length);
-guessedPlacesVisited = prompt('You will have ' + placesVisited.length + ' chances to guess countries I have been to. Please make your first guess of a country I have been to?').toLowerCase();
+guessedPlacesVisited = prompt('You will have ' + placesVisited.length + ' chances to guess countries I have visited, one at a time. Please make your first guess of a country I have visited?').toLowerCase();
 //while(loopCounter < placesVisited.length){
 for(var loopCount = 0; loopCount < placesVisited.length; loopCount++){
   //console.log('guess of place visited:',guessedPlacesVisited);
   for(var j=0; j < placesVisited.length; j++) {
     //console.log('traversing array:', placesVisited[i]);
     if(guessedPlacesVisited === placesVisited[j]) {
-      alert('Welldone, ' + userName + '. That\'s one of them!!!');
+      alert('Welldone, ' + userName + '. Yes, I have been to ' + guessedPlacesVisited + '!!!');
       tally = tally + 1;
       console.log('tally value:',tally);
       //break;
@@ -113,15 +113,17 @@ for(var loopCount = 0; loopCount < placesVisited.length; loopCount++){
   }
   console.log('a place visited:', placesVisited[j]);
   console.log('guessedPlacesVisited:',guessedPlacesVisited);
-  guessedPlacesVisited = prompt('Please guess another country I have been to, you have '+ --chances + ' chances to go.').toLowerCase();
+  while(chances > 0){
+    guessedPlacesVisited = prompt('Please guess another country I have been to, you have '+ --chances + ' chances to go.').toLowerCase();
+  }
 }
 
 alert(userName + ' you got ' + tally + ' out of ' + placesVisited.length + ' correct');
 
 
-if (window.confirm('Hope you enjoyed answering these questions.')) {
+if (window.confirm('Thanks for taking part, hope you enjoyed engaging with me.')) {
   console.log('use of confirm');
-  window.open('exit.html', 'Thanks for playing!');
+  window.open('exit.html', ' Thanks for playing!');
 }
 
 //trying something new trying out getElementById
